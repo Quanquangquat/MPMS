3e109cd8e8c1e5e439c4db777f79856f334a4c94
admin <admin@10.33.41.29>
2024-10-28 18:59:37 +0700

    Merge branch 'main' of https://github.com/Quanquangquat/MPMS into task
    # Conflicts:
    #	src/java/Model/User.java

--- src/java/controller/AllocationController.java
+++ src/java/controller/AllocationController.java
@@ -18,10 +18,6 @@ import jakarta.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.util.Vector;
 
-/**
- *
- * @author admin
- */
 @WebServlet(name = "AllocationController", urlPatterns = {"/AllocationController"})
 public class AllocationController extends HttpServlet {
 
