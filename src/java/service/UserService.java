47ad0c4a2b07fcf58e3c758ad4caf10d41a98fce
Q180197HE <quanpdmhe180197@fpt.edu.vn>
2024-11-01 09:23:52 +0700

    Merge branch 'master' of https://github.com/Quanquangquat/MPMS Signed-off-by:Q180197HE <quanpdmhe180197@fpt.edu.vn>

--- src/java/service/UserService.java
+++ src/java/service/UserService.java
@@ -1,33 +1,22 @@
 package service;
 
-import static com.sun.source.util.DocTrees.instance;
-import static com.sun.source.util.JavacTask.instance;
-import static com.sun.source.util.Trees.instance;
 import dao.UserDAO;
 import model.User;
-
 import java.util.List;
 
 public class UserService {
-
-    public static UserService getInstance() {
-        UserService instance = null;
-if (instance == null) {
-        instance = new UserService();
-    }
-    return instance;    }
     private UserDAO userDAO;
 
     public UserService() {
-        userDAO = new UserDAO();
-    }
-
-    public void registerUser(User user) {
-        userDAO.insertUser(user);
+        this.userDAO = new UserDAO();
     }
 
     public User loginUser(String email, String password) {
         return userDAO.getUserByEmailAndPassword(email, password);
+    }
+
+    public void registerUser(User user) {
+        userDAO.insertUser(user);
     }
 
     public List<User> getAllUsers() {
